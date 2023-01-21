d873e7ffc909a32d34a6494cb9847bcda3aac961
justin <justinfrost@duck.com>
2023-01-20 23:57:44 -0700

    init: clj project codegen using deps-new

---
+++ src/monadica/monadica.clj
@@ -0,0 +1,22 @@
+(ns monadica.monadica
+  (:gen-class))
+
+;; Outlining goal of monadica
+;; - lightweight clojure webhosting
+;; - continuous integration
+;; - live repl/testing
+;; - devops / orchestration via clojure
+;; - when I say clojure I really mean babashka
+;; - emacs plugin
+;; - ring / malli / reitit / clojure library glue defaults
+;; - xtdb - datalog - graph db
+;; - business rules
+;; - domain design schema
+;; - component/donut style componentization
+;; - tree-sitter / zipper / fast hiccup patching
+;; - static main site / blog options
+;; - and many more on channel 4
+
+(defn -main
+  [& args]
+  (println "Blorp."))
---
+++ test/monadica/monadica_test.clj
@@ -0,0 +1,7 @@
+(ns monadica.monadica-test
+  (:require [clojure.test :refer :all]
+            [monadica.monadica :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
